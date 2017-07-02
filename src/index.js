03b0449fb357ba04175e34aec19593e78b740ea2
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-07-02 17:54:00 -0300

    Add comments

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-module.exports = class Busx {
+/**
+ * @class Busx
+ * @author Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
+ * @version 0.1.0
+ */
+class Busx {
   constructor () {
     this.handlers = []
   }
 
+  /**
+   * Listen for events
+   *
+   * @param {any} key
+   * @param {any} handler
+   * @memberof Busx
+   */
   listen (key, handler) {
     this.handlers.push({
       key,
@@ -10,13 +22,28 @@ module.exports = class Busx {
     })
   }
 
+  /**
+   * Fire the handlers for the event
+   *
+   * @param {any} key
+   * @param {any} [data={}]
+   * @memberof Busx
+   */
   fire (key, data = {}) {
     this.handlers
       .filter(handler => handler.key === key)
       .forEach(handler => handler.handler(data))
   }
 
+  /**
+   * Return a list with all registered events
+   *
+   * @returns
+   * @memberof Busx
+   */
   all () {
     return this.handlers
   }
 }
+
+module.exports = Busx
